2a865cc11a1ac2bdb66b620d76c12e3ababc55de
icyveins7 <icyveins@hotmail.com>
2025-02-25 18:11:54 +0800

    add 2nd function to module1 and use numpy arrays as arguments

--- src/module1.py
+++ src/module1.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def arange(start, end):
     return np.arange(start, end)
+
+def addarrs(arr1, arr2):
+    return arr1 + arr2
--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,9 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
+#include <pybind11/numpy.h>
 #include "general.h"
 
+#include <vector>
 #include <iostream>
 
 namespace py = pybind11;
@@ -25,6 +27,15 @@ int main(){
   // since the .py file is probably not copied to the build folder
   auto result = pct::runPythonFunction("module1", "arange", 0, 3);
   py::print(py::str(result));
+
+  py::array_t<double, py::array::c_style> x(3);
+  auto xa = x.mutable_unchecked();
+  xa(0) = 1.0; xa(1) = 2.0; xa(2) = 3.0;
+  py::array_t<double, py::array::c_style> y(3);
+  auto ya = y.mutable_unchecked();
+  ya(0) = 1.0; ya(1) = 4.0; ya(2) = -1.0;
+  auto result2 = pct::runPythonFunction("module1", "addarrs", x, y);
+  py::print(py::str(result2));
 
   auto randresult = pct::runPythonFunction("module2", "rand", 5);
   py::print(py::str(randresult));
