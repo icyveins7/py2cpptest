1dbea9c702a78f846975ee438b40766da3488172
icyveins7 <icyveins@hotmail.com>
2025-02-26 11:51:32 +0800

    add an example_gtest to show how to use with GoogleTest
    the cmakelists is also updated accordingly (cannot use gtest_main since
    we need to start interpreter)

---
+++ src/example_gtest.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+
+#include "general.h"
+
+namespace py = pybind11;
+
+template <typename T>
+using numpy_array = py::array_t<T, py::array::c_style | py::array::forcecast>;
+
+TEST(ExampleGTest, Arange){
+  numpy_array<int64_t> x = pct::runPythonFunction("module1", "arange", 0, 3);
+  std::vector<int64_t> xc = pct::fromNumpyArray(x);
+
+  std::vector<int64_t> y = {0, 1, 2};
+
+  for (size_t i = 0; i < xc.size(); i++){
+    EXPECT_EQ(xc[i], y[i]);
+  }
+
+}
+
+TEST(ExampleGTest, AddArrs){
+  std::vector<float> x = {1.0, 2.0, 3.0};
+  std::vector<float> y = {1.0, 4.0, -1.0};
+  numpy_array<float> z = pct::runPythonFunction("module1", "addarrs", pct::toNumpyArray(x), pct::toNumpyArray(y));
+
+  std::vector<float> zc = pct::fromNumpyArray(z);
+  std::vector<float> expected = {2.0, 6.0, 2.0};
+
+  for (size_t i = 0; i < zc.size(); i++){
+    EXPECT_EQ(zc[i], expected[i]);
+  }
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+
+  // Start the interpreter!
+  printf("Starting the python interpreter for example_gtest...");
+  py::scoped_interpreter guard{};
+  printf("Done\n");
+
+  return RUN_ALL_TESTS();
+}
